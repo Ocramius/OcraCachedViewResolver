a4eab77a11700e5b3c3916f055fbbe892bd42648
Marco Pivetta <ocramius@gmail.com>
2012-11-30 00:48:54 +0100

    Removing test configuration

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -19,7 +19,3 @@
 if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !@(include_once __DIR__ . '/../../../autoload.php')) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
-
-if (!$config = @include __DIR__ . '/TestConfiguration.php') {
-    $config = require __DIR__ . '/TestConfiguration.php.dist';
-}
