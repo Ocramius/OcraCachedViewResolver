22fdcacacea3578c87790175580a909f0ea6467f
Marco Pivetta <ocramius@gmail.com>
2013-05-31 22:01:59 +0200

    Fixing test bootstrapper (no silence operator)

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -16,6 +16,32 @@
  * and is licensed under the MIT license.
  */
 
-if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !@(include_once __DIR__ . '/../../../autoload.php')) {
-    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+ini_set('error_reporting', E_ALL);
+
+$files = array(__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php');
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $loader = require $file;
+
+        break;
+    }
 }
+
+if (! isset($loader)) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you install via composer?');
+}
+
+$loader->add('ZfrRestTest\\', __DIR__);
+
+$configFiles = array(__DIR__ . '/TestConfiguration.php', __DIR__ . '/TestConfiguration.php.dist');
+
+foreach ($configFiles as $configFile) {
+    if (file_exists($configFile)) {
+        $config = require $configFile;
+
+        break;
+    }
+}
+
+unset($file, $file, $loader, $configFiles, $configFile, $config);
