c2ff3f8d29d5487596cdf0fbe47b906c8a59d662
Marco Pivetta <ocramius@gmail.com>
2014-05-11 15:07:21 +0200

    Coverage for the invalid resolver instantiator exception

---
+++ tests/OcraCachedViewResolverTest/View/Resolver/Exception/InvalidResolverInstantiatorExceptionTest.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace OcraCachedViewResolverTest\View\Resolver\Exception;
+
+use OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Tests for {@see \OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException}
+ *
+ * @author  Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ *
+ * @covers \OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException
+ * @covers \OcraCachedViewResolver\View\Resolver\Exception\ExceptionInterface
+ */
+class InvalidResolverInstantiatorExceptionTest extends PHPUnit_Framework_TestCase
+{
+    public function testInstanceOfBaseExceptionInterface()
+    {
+        $exception = new InvalidResolverInstantiatorException();
+
+        $this->assertInstanceOf('OcraCachedViewResolver\View\Resolver\Exception\ExceptionInterface', $exception);
+    }
+
+    public function testFromInvalidNullInstantiator()
+    {
+        $exception = InvalidResolverInstantiatorException::fromInvalidInstantiator(null);
+
+        $this->assertInstanceOf(
+            'OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException',
+            $exception
+        );
+
+        $this->assertSame(
+            'Invalid instantiator given, expected `callable`, `NULL` given.',
+            $exception->getMessage()
+        );
+    }
+
+    public function testFromInvalidObjectInstantiator()
+    {
+        $exception = InvalidResolverInstantiatorException::fromInvalidInstantiator($this);
+
+        $this->assertInstanceOf(
+            'OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException',
+            $exception
+        );
+
+        $this->assertSame(
+            'Invalid instantiator given, expected `callable`, `' . __CLASS__ . '` given.',
+            $exception->getMessage()
+        );
+    }
+
+    public function testFromInvalidNullResolver()
+    {
+        $exception = InvalidResolverInstantiatorException::fromInvalidResolver(null);
+
+        $this->assertInstanceOf(
+            'OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException',
+            $exception
+        );
+
+        $this->assertSame(
+            'Invalid resolver found, expected `Zend\View\Resolver\ResolverInterface`, `NULL` given.',
+            $exception->getMessage()
+        );
+    }
+
+    public function testFromInvalidObjectResolver()
+    {
+        $exception = InvalidResolverInstantiatorException::fromInvalidResolver($this);
+
+        $this->assertInstanceOf(
+            'OcraCachedViewResolver\View\Resolver\Exception\InvalidResolverInstantiatorException',
+            $exception
+        );
+
+        $this->assertSame(
+            'Invalid resolver found, expected `Zend\View\Resolver\ResolverInterface`, `' . __CLASS__ . '` given.',
+            $exception->getMessage()
+        );
+    }
+}
