e831bde4a84d181bbd5c433fe6255fa8f3d6436c
Marco Pivetta <ocramius@gmail.com>
2014-05-11 15:16:48 +0200

    Coverage for the module class

---
+++ tests/OcraCachedViewResolverTest/ModuleTest.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace OcraCachedViewResolverTest;
+
+use OcraCachedViewResolver\Module;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Tests for {@see \OcraCachedViewResolver\Module}
+ *
+ * @author  Marco Pivetta <ocramius@gmail.com>
+ * @license MIT
+ *
+ * @covers \OcraCachedViewResolver\Module
+ */
+class ModuleTest extends PHPUnit_Framework_TestCase
+{
+    public function testConfigIsAnArray()
+    {
+        $module = new Module();
+
+        $this->assertInternalType('array', $module->getConfig());
+    }
+
+    public function testConfigIsSerializable()
+    {
+        $module = new Module();
+
+        $this->assertSame($module->getConfig(), unserialize(serialize($module->getConfig())));
+    }
+}
